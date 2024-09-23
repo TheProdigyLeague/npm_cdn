63ebdd85d0000c7757842be2713f4f9c44687cff
Z4xk3r0th <30985576+TheProdigyLeague@users.noreply.github.com>
2024-09-23 12:54:59 -0500

    :last_quarter_moon_with_face:
    🌝

---
+++ dist/npm_CDN-debugging.js
@@ -0,0 +1,55 @@
+// ecosia google
+search?q=https%3A%2F%2Fregistry.npmjs.org%2F%40babel%2Fplugin-transform-modules-amd%2F-%2Fplugin-transform-modules-amd-7.16.7.tgz&addon=opensearch
+srcURL="https://syndicatedsearch.goog/afs/ads/i/iframe.html:formatted"
+▶ iframe.html:formatted // frame loading
+⮕  var masterFrame = getMaster();
+            try {
+                masterFrame && masterFrame.mPAASH && masterFrame.mPAASH();
+            } catch (e) {}
+❌ DOMException: Blocked a frame with origin // google fighting frame
+▶ Protocol - 127.0.0.1
+▶ [TLS]: 1.3 // outdated versions
+▶ [KEG]: X25519 // invalid sigs
+▶ Cipher - CHACHA20_POLY1305
+⮕ *.appengine.google.com/ce79b05.js:formatted
+⮕ f = function(e, t) {
+                if (!t)
+                    return e;
+                try {
+                    return JSON.parse(e)
+                  ❌ Exception: SyntaxError: Unexpected token a in JSON at position 0 at JSON.parse (<anonymous>) at f (srcURL="https://www.ecosia.org/static/web/ce79b05.js:13:130644) at Object.get (https://www.ecosia.org/static/web/ce79b05.js:13:131517) at p (https://www.ecosia.org/static/web/ce79b05.js:13:120726) at Pc (https://www.ecosia.org/static/web/ce79b05.js:46:290) at https://www.ecosia.org/static/web/ce79b05.js:46:1673 at Set.forEach (<anonymous>) at Hc (https://www.ecosia.org/static/web/ce79b05.js:46:1574) at https://www.ecosia.org/static/web/ce79b05.js:46:2409 at a.e.$track (https://www.ecosia.org/static/web/ce79b05.js:56:3101") // loading results from index
+this: undefined
+e: "a=1:as=1:cs=0:dt=pc:f=i:fr=0:fs=1:l=en:lt=1727113150889:mc=en-us:nf=1:nt=0:pz=0:t=33:tt=0:tu=auto:wu=auto:ma=1"
+t: true
+                  Promise.then async // 与应用程序编程接口异步
+                     t.push(s[2] = c);
+                var u, l = document.createElement("script");
+                l.charset = "utf-8",
+                l.timeout = 120,
+                a.nc && l.setAttribute("nonce", a.nc),
+                l.src = i(e);
+                var d = new Error;
+                u = function(t) {
+                    l.onerror = l.onload = null,
+                    clearTimeout(f);
+                    var n = o[e];
+                    if (0 !== n) {
+                        if (n) {
+                            var r = t && ("load" === t.type ? "missing" : t.type)
+                              , i = t && t.target && t.target.src;
+                            d.message = "Loading chunk " + e + " failed.\n(" + r + ": " + i + ")",
+                            d.name = "ChunkLoadError",
+                            d.type = r,
+                            d.request = i,
+                            n[1](d)
+                        }
+                        o[e] = void 0
+                    }
+                }
+                ;
+                  break
+                  
+                  ⮕ ads.js?pac=0:formatted
+                  ❌ TypeError: Cannot read properties of undefined (reading... 'requestContent')
+                    // 刷新引导加载跨域负载程序
+  // eof
